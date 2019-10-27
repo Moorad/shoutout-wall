a52ea4f36cc174b7ec4133f66056b811e00d1515
Moorad <34781684+Moorad@users.noreply.github.com>
2019-10-27 16:52:23 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+var CONFIG = {
+
+	// Update the below keys as required
+	"nightbotToken" : "XXX", // Add your !quote token from nightbot custom quote API here
+	"googleApiKeys" : ["XXX"], // Add your google API key/s here
+	"nightbotPrefix" : /!XXX/gi, // Change zzz to align with whatever prefix you've set in your !wall command
+
+	// Add channelID to use google API for Live Chat rather than Nightbot
+	"channelID" : "XXX", // Enter your youtube channelID here
+
+	// Customise the size of your wall here
+	"numberOfChannels" :20 ,
+	"numberOfRows" : 4 
+};
